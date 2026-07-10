c40dc98840ba3f80fdb9643dcc29e526e770b543
DigitalCyberSoft <support@digitalcybersoft.com>
2026-07-10 11:05:56 +0800

    Bump version to 0.4.0
    GNOME (Xorg) support via the compositor abstraction, the cinnamon-screensaver
    fakexrandr override, and the get_active=1 single-monitor lock coverage fix.

--- splitrandr/meta.py
+++ splitrandr/meta.py
@@ -17,7 +17,7 @@
 from .i18n import _
 
 
-__version__ = '0.3.3'
+__version__ = '0.4.0'
 PROGRAMNAME = _('SplitRandR')
 PROGRAMDESCRIPTION = _('Monitor Layout Editor with Virtual Monitor Splitting')
 COPYRIGHT = 'Based on ARandR by chrysn 2008-2019'
